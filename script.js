e8e4da297857a583889a4cc7d1da32a89c8037d3
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2020-11-20 01:06:30 +0530

    added edit and del buttons

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ function add() {
   if (inp.value !== "") {
     const div = document.createElement("div");
     const p = document.createElement("p");
+    const edit = document.createElement("input");
+    const del = document.createElement("input");
+
+    edit.setAttribute("id", "edit");
+    edit.setAttribute("type", "button");
+    edit.setAttribute("onclick", "edit()");
+    edit.setAttribute("value", "Edit");
+
+    del.setAttribute("id", "del");
+    del.setAttribute("type", "button");
+    del.setAttribute("onclick", "del()");
+    del.setAttribute("value", "Delete");
     p.innerText = inp.value;
 
     div.appendChild(p);
+    div.appendChild(edit);
+    div.appendChild(del);
     mainDiv.appendChild(div);
     document.body.appendChild(mainDiv);
     inp.value = "";
   }
+}
+
+function del() {
+  event.target.();
 }
